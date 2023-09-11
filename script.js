afa40f71f6b2fe0c36086c73bb31cbdd7a42dd65
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2023-09-11 18:45:17 +0300

    Update: add new projects

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 const works = [
   {
-    id: 0,
-    imgLink: "assets/works/Frontend-Mentor-FAQ-Accordion-Card.png",
-    title: "FAQ Accordion",
-    desc: "FAQ accordion is a collapsible UI element for displaying frequently asked questions. It's built using HTML, CSS, and JavaScript for a user-friendly experience.",
-    category: "HTML & CSS",
-    demoLink:
-      "https://abdelrhmanehab10.github.io/frontend-mentor/faq-accordion-card/",
-  },
-  {
     id: 1,
     imgLink: "assets/works/React-App.png",
     title: "Food menu & cart",
     desc: "Is a dynamic and interactive way to display menu items, add to cart and process orders. It utilizes the power of React's component-based architecture and state management for a seamless user experience.",
     category: "REACT JS",
-    demoLink:
-      "https://abdelrhmanehab10.github.io/food-app/",
+    demoLink: "https://abdelrhmanehab10.github.io/food-app/",
   },
   {
     id: 2,
-    imgLink: "assets/works/Tabs-Project.png",
-    title: "Tabs project",
-    desc: "Tabs project built with Next js is a simple yet effective way to organize content into tabs. It leverages the power of Next js for server-side rendering and optimized performance.",
+    imgLink: "assets/works/SwiftMarket.png",
+    title: "E-Commerce",
+    desc: "E-Commerce built with Next js. It leverages the power of Next js for server-side rendering and optimized performance.",
     category: "NEXT JS",
-    demoLink:
-      "https://nextjs-swart-delta.vercel.app/",
+    demoLink: "https://e-commerce-nine-self.vercel.app/",
   },
 ];
 
